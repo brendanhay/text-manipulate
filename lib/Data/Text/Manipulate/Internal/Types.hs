e94640f41325fe651ff381131de69d2ba5718195
Brendan Hay <brendan.g.hay@gmail.com>
2022-03-20 07:54:28 +0100

    Bump copyright year

--- lib/Data/Text/Manipulate/Internal/Types.hs
+++ lib/Data/Text/Manipulate/Internal/Types.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE ViewPatterns #-}
 
 -- Module      : Data.Text.Manipulate.Internal.Types
--- Copyright   : (c) 2014-2020 Brendan Hay <brendan.g.hay@gmail.com>
+-- Copyright   : (c) 2014-2022 Brendan Hay <brendan.g.hay@gmail.com>
 -- License     : This Source Code Form is subject to the terms of
 --               the Mozilla Public License, v. 2.0.
 --               A copy of the MPL can be found in the LICENSE file or
